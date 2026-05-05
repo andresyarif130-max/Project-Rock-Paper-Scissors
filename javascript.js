d7e9107af0692687d03b6ae2edd7bd62ec24b59f
Andre Syarif <andresyarif130@gmail.com>
2026-05-05 12:25:15 +0800

    plan pseudocode

--- javascript.js
+++ javascript.js
@@ -1,1 +1,6 @@
-console.log("Hi World")
+// Step 2 pseudocode 
+ // Create new function getComputerChoice.
+ // Create 1 var inside it with Math.floor & Math.random
+ // Create if... else statement for rock, paper, scissors
+
+// Step 2
